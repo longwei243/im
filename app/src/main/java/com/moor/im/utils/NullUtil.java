ae3d6ba7c782a45a93b16b92a65c7093183e0750
longwei <liulongwei243@yeah.net>
2015-10-30 14:58:08 +0800

    bug fix

---
+++ app/src/main/java/com/moor/im/utils/NullUtil.java
@@ -0,0 +1,21 @@
+package com.moor.im.utils;
+
+/**
+ * 检测值是否为空
+ * Created by longwei on 2015/10/30.
+ */
+public class NullUtil {
+
+    /**
+     * 检测字符串是否是空的，若是空的则返回空字符串，避免空指针错误
+     * @param str
+     * @return
+     */
+    public static String checkNull(String str) {
+            if(str != null && !"".equals(str)) {
+                return str;
+            }else {
+                return "";
+            }
+    }
+}
